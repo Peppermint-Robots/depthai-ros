15938f33c1214497bbb2a0737d29c37b890880fa
tanaya-peppermint <tanaya.gupte@getpeppermint.co>
2023-05-27 10:53:15 +0530

    made changes to launch multiple cams at once

--- depthai_ros_driver/launch/example_multicam.launch.py
+++ depthai_ros_driver/launch/example_multicam.launch.py
@@ -1,4 +1,5 @@
 import os
+import yaml
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
@@ -11,49 +12,72 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 
 
-
 def launch_setup(context, *args, **kwargs):
-
     depthai_prefix = get_package_share_directory("depthai_ros_driver")
     params_file = os.path.join(depthai_prefix, "config", "multicam_example.yaml")
-    # put mx_ids here
-    cams = ["oak_d_w", "oak_d_lite"]
+    """Specify cameras used on the robot
+
+    Returns:
+        _type_: _description_
+    """
+
+    cams = []
+    with open(params_file, "r") as file:
+        params = yaml.full_load(file)
+    for i in params.keys():
+        print("cam name: ", i)
+        cams.append(i)
+    # cams = ["front_camera", "left_camera", "right_camera"]
     nodes = []
-    i=0.0
+    i = 0.0
+
+    """Launch all specified cameras
+
+    Returns:
+        _type_: _description_
+    """
     for cam_name in cams:
         node = IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
-                os.path.join(depthai_prefix, 'launch', 'camera.launch.py')),
-            launch_arguments={"name": cam_name,
-                              "parent_frame": "map",
-                              "params_file": params_file,
-                              "cam_pos_y": str(i)}.items())
+                os.path.join(depthai_prefix, "launch", "camera.launch.py")
+            ),
+            launch_arguments={
+                "name": cam_name,
+                "parent_frame": "map",
+                "params_file": params_file,
+                "cam_pos_y": str(i),
+            }.items(),
+        )
         nodes.append(node)
-        i=i+0.1
+        i = i + 0.1
+
     spatial_rgbd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(depthai_prefix, 'launch',
-                         'rgbd_pcl.launch.py')
+            os.path.join(depthai_prefix, "launch", "rgbd_pcl.launch.py")
         ),
-        launch_arguments={"name": "oak_d_pro",
-                          "parent_frame": "map",
-                          "params_file": params_file,
-                          "cam_pos_y": str(-0.1)}.items())
+        launch_arguments={
+            "name": "oak_d_pro",
+            "parent_frame": "map",
+            "params_file": params_file,
+            "cam_pos_y": str(-0.1),
+        }.items(),
+    )
 
     obj_det = Node(
         package="depthai_ros_driver",
         executable="obj_pub.py",
-        remappings=[("/oak/nn/detections", "/oak_d_pro/nn/detections"),
-        ("/oak/nn/detection_markers", "/oak_d_pro/nn/detection_markers")]
+        remappings=[
+            ("/oak/nn/detections", "/oak_d_pro/nn/detections"),
+            ("/oak/nn/detection_markers", "/oak_d_pro/nn/detection_markers"),
+        ],
     )
 
-    nodes.append(spatial_rgbd)
-    nodes.append(obj_det)
+    # nodes.append(spatial_rgbd)
+    # nodes.append(obj_det)
     return nodes
 
 
 def generate_launch_description():
-
     return LaunchDescription(
         [
             OpaqueFunction(function=launch_setup),
